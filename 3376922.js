f4229540e60b50d610bd1b017f251c9d80e74072
asarenana103 <134610510+asarenana103@users.noreply.github.com>
2025-06-11 10:14:33 +0000

    Create 3376922.js

---
+++ 3376922.js
@@ -0,0 +1,32 @@
+class TrieNode:
+    def _init_(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def _init_(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
